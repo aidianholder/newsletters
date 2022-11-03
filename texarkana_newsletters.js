e855cb571b595f05ce410f187704ccf969b00fef
Aidian Holder <aidianholder@gmail.com>
2022-11-03 08:24:33 -0500

    namespaced script, added TD push

---
+++ texarkana_newsletters.js
@@ -0,0 +1,10 @@
+let newsletterDetails =
+    [{"newsletterName":"Breaking News Email - Texarkana","subscriptionGroupID":"ba38584b-fef6-41f0-a9d9-28c6e1964dd2","newsletterCopy":null},
+    {"newsletterName":"Crime and Courts","subscriptionGroupID":"7a284c63-09b0-44e9-82ea-a1079864b81f","newsletterCopy":null},
+    {"newsletterName":"Featured Features","subscriptionGroupID":"95c7ae63-c021-45d8-ae6a-33c7c57d90e3","newsletterCopy":null},
+    {"newsletterName":"HER Magazine Newsletter","subscriptionGroupID":"273101e4-1708-44f0-82ea-7dde7f9647f3","newsletterCopy":null},
+    {"newsletterName":"Morning Email Update - TX","subscriptionGroupID":"535f241c-a020-4cd5-97fc-5b17f2a26590","newsletterCopy":null},
+    {"newsletterName":"Opinion Roundup","subscriptionGroupID":"ac846905-f6f7-49e7-bd19-b299670a620c","newsletterCopy":null},
+    {"newsletterName":"Sports Email","subscriptionGroupID":"70f30b05-42bf-4376-af6a-2dafd90c4580","newsletterCopy":null},
+    {"newsletterName":"The Upside","subscriptionGroupID":"5ef39a44-4895-4460-97be-10964f37e1f0","newsletterCopy":null},
+    {"newsletterName":"Weekend Accent","subscriptionGroupID":"597fd44b-5356-423e-a6c1-9bb2a9716a2b","newsletterCopy":null}]
